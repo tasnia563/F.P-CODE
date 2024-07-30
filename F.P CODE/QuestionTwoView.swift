418fbf71301dafb931452c17ce2a61d2a81df623
Tasnia Rafa <tasniaxrafa@gmail.com>
2024-07-30 11:17:16 -0400

    Finial Q App Project

---
+++ F.P CODE/QuestionTwoView.swift
@@ -0,0 +1,18 @@
+//
+//  QuestionTwoView.swift
+//  F.P CODE
+//
+//  Created by Scholar on 7/30/24.
+//
+
+import SwiftUI
+
+struct QuestionTwoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuestionTwoView()
+}
